a4f17cc7931f94722c10342ea5a0ba3c6d4940e8
stevchen99 <stevchen99@gmail.com>
2022-06-09 15:38:54 +0200

    3 files modified

--- src/app/job/job-serv.service.ts
+++ src/app/job/job-serv.service.ts
@@ -1,8 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TheJob } from './the-job';
 
 @Injectable()
 export class JobServService {
   ApiPHP = 'https://le-esp.fr/CRM/';
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+
+  getJobs(): Observable<TheJob[]> {
+    return this.httpClient.get<TheJob[]>(`${this.ApiPHP}/JobRead.php`);
+  }
+
+  CreateJob(job: TheJob): Observable<TheJob> {
+    return this.httpClient.post<TheJob>(`${this.ApiPHP}/JobCreate.php`, job);
+  }
 }
