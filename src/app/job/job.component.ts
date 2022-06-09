bb00b43c63752479acfafb4906757d7ad65f912a
stevchen99 <stevchen99@gmail.com>
2022-06-09 15:38:54 +0200

    3 files modified

--- src/app/job/job.component.ts
+++ src/app/job/job.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { JobServService } from './job-serv.service';
+import { TheJob } from './the-job';
 
 @Component({
   selector: 'app-job',
   templateUrl: './job.component.html',
-  styleUrls: ['./job.component.css']
+  styleUrls: ['./job.component.css'],
 })
 export class JobComponent implements OnInit {
+  Jobs: TheJob[] = [];
 
-  constructor() { }
+  constructor(private apiService: JobServService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getJobs() {
+    this.apiService.getJobs().subscribe((tempo: TheJob[]) => {
+      this.Jobs = tempo;
+      console.log(tempo);
+    });
   }
-
 }
